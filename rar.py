eab8383874ebfe2ef2464a1e8ef4e910c4744a48
rjne123 <83156021+rjne123@users.noreply.github.com>
2021-05-16 13:15:30 +0500

    Update rar.py

--- rar.py
+++ rar.py
@@ -34,42 +34,6 @@ class GameSprite1(sprite.Sprite):
         press = key.get_pressed()
         if press[K_LEFT]:
             self.rect.y += 5
-from pygame import* 
-from random import*
-
-window = display.set_mode((700,700))
-
-game = True
-clock = time.Clock()
-FPS = 60
-class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x,player_y):
-        super().__init__()
-        self.image = transform.scale(image.load(player_image), (50,50))
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-    def dv(self):
-        press = key.get_pressed()
-        if press[K_UP]:
-            self.rect.y += 5
-        if press[K_DOWN]:
-            self.rect.y -= 5
-class GameSprite1(sprite.Sprite):
-    def __init__(self, player_image, player_x,player_y):
-        super().__init__()
-        self.image = transform.scale(image.load(player_image), (50,50))
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-    def dv(self):
-        press = key.get_pressed()
-        if press[K_LEFT]:
-            self.rect.y += 5
         if press[K_RIGHT]:
             self.rect.y -= 5
 class GameSprite2(sprite.Sprite):
@@ -94,12 +58,10 @@ ll = GameSprite('12838980299.jpg',100,350)
 ll1 = GameSprite1('12838980299.jpg',600,350)
 lll = GameSprite('12838980299.jpg',100,600)
 lll1 = GameSprite1('12838980299.jpg',600,600)
-if sc1 == 10:
-    ball.hide()
-    print("правая ракетка победила")
-if sc2 == 10:
-    ball.hide()
-    print("левая ракетка победила")
+
+
+k1 = transform.scale(image.load("game-over-neon-sign_1174-671.jpg"), (700,700))
+win =False
 while game:
     ball.rect.x +=dx
     ball.rect.y -=dy
@@ -114,10 +76,12 @@ while game:
     if ball.rect.y > 650:
         dy *= -1
     if sc1 == 10:
-        ball.hide()
+        win = True
+        game=False
         print("правая ракетка победила")
     if sc2 == 10:
-        ball.hide()
+        win = True
+        game=False
         print("левая ракетка победила")
     window.fill((10,10,100))
     for i in event.get():
@@ -131,6 +95,7 @@ while game:
     ll.dv()
     ll1.reset()
     ll1.dv()
+
     lll.reset()
     lll.dv()
     lll1.reset()
@@ -138,7 +103,7 @@ while game:
     ball.reset()
     if sprite.collide_rect(ball, l):
         dx *= -1
-    if sprite.collide_rect(ball, l1):
+    if sprite.collide_rect(ball, l1):         
         dx *= -1
     if sprite.collide_rect(ball, ll):
         dx *= -1
@@ -148,5 +113,13 @@ while game:
         dx *= -1
     if sprite.collide_rect(ball, lll1):
         dx *= -1
+    clock.tick(30)
+    display.update()
+
+while win:
+    window.blit(k1,(0,0))
+    for i in event.get():
+        if i.type==QUIT:
+            win = False
     clock.tick(30)
     display.update()
